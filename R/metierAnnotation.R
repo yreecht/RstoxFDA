f0122548f02b10a16029543e662e536531ead3fd
edvinf <edvin.fuglebakk@gmail.com>
2020-06-24 17:21:08 +0200

    Redifned format and added some tests, and stop-reminders

--- R/metierAnnotation.R
+++ R/metierAnnotation.R
@@ -27,14 +27,13 @@
 #'   \item{metier}{character() metier-string, e.g.: OTB_DEF_>=120_0_0_all}
 #'   \item{gearcode}{character() encoding gear}
 #'   \item{target}{character(), optional, target species}
-#'   \item{meshedGear}{logical(), optional, whether the gear is a meshed gear}
+#'   \item{meshedGear}{logical(), optional, whether the gear is a meshed gear. Should be provided for all or none gears}
 #'   \item{lowerMeshSize}{integer(), optional, the lower mesh size to include in this metier.}
 #'   \item{upperMeshSize}{integer(), optional, the upper mesh size to include in this metier.}
 #'   \item{selectivityDevice}{character(), optional, encoding selectivity device.}
-#'   \item{meshedSelectivityDevice}{logical(), optional, encoding selectivity device.}
+#'   \item{meshedSelectivityDevice}{logical(), optional, encoding selectivity device. Should be provided for all or none selectivity devices}
 #'   \item{selDevLowerMeshSize}{integer(), optional, the lower mesh size of selectivity device to include in this metier.}
 #'   \item{selDevUpperMeshSize}{integer(), optional, the upper mesh size of selectivity device to include in this metier.}
-#'   \item{AREA}{character(), optional, encoding any area the metier is restricted to. NA indicates that the metier in defined for all areas.}
 #'  }
 #'
 #'  The metier-defining parameters are written in camelCase, parameters that may be used to distinguish applicability of different metierdefinitions are writter in UPPER case.
@@ -43,44 +42,79 @@
 #'
 NULL
 
-is.MetierTable <- function(table){
+#' Check if table is correctly formatted metier table
+#' @param table
+#' @param throwError if set errors are raised, if not, validity will be returned as T/F
+#' @return validity
+#' @noRd
+is.MetierTable <- function(table, throwError=F){
 
-  if (!is.data.table(table)){
+  if (!data.table::is.data.table(table)){
+    if (throwError){
+      stop("A metiertable must be a data.table")
+    }
     return(FALSE)
   }
-  if (!all(c("metier", "gearcode", "target", "meshedGear", "lowerMeshSize", "upperMeshSize", "selectivityDevice", "meshedSelectivityDevice", "selDevLowerMeshSize", "selDevUpperMeshSize", "AREA") %in% names(table))){
+  if (!all(c("metier", "gearcode", "target", "meshedGear", "lowerMeshSize", "upperMeshSize", "selectivityDevice", "meshedSelectivityDevice", "selDevLowerMeshSize", "selDevUpperMeshSize") %in% names(table))){
+    if (throwError){
+      stop("Metiertable does not have the required columns")
+    }
     return(FALSE)
   }
-  if (is.character(table$metier)){
+  if (!is.character(table$metier)){
+    if (throwError){
+      stop("The column 'metier' must be a character")
+    }
     return(FALSE)
   }
-  if (is.character(table$gearcode)){
+  if (!is.character(table$gearcode)){
+    if (throwError){
+      stop("The column 'gearcode' must be a character")
+    }
     return(FALSE)
   }
-  if (is.logical(table$meshedGear)){
+  if (!is.logical(table$meshedGear)){
+    if (throwError){
+      stop("The column 'meshedGear' must be a logical")
+    }
     return(FALSE)
   }
-  if (is.integer(table$lowerMeshSize)){
+  if (!is.integer(table$lowerMeshSize)){
+    if (throwError){
+      stop("The column 'lowerMeshSize' must be an integer")
+    }
+  }
+  if (!is.integer(table$upperMeshSize)){
+    if (throwError){
+      stop("The column 'upperMeshSize' must be an integer")
+    }
     return(FALSE)
   }
-  if (is.integer(table$upperMeshSize)){
+  if (!is.character(table$selectivityDevice)){
+    if (throwError){
+      stop("The column 'selectivityDevice' must be a character")
+    }
     return(FALSE)
   }
-  if (is.character(table$selectivityDevice)){
+  if (!is.logical(table$meshedSelectivityDevice)){
+    if (throwError){
+      stop("The column 'meshedSelectivityDevice' must be a logical")
+    }
     return(FALSE)
   }
-  if (is.logical(table$meshedSelectivityDevice)){
+  if (!is.integer(table$selDevLowerMeshSize)){
+    if (throwError){
+      stop("The column 'selDevLowerMeshSize' must be an integer")
+    }
+  }
+  if (!is.integer(table$selDevUpperMeshSize)){
+    if (throwError){
+      stop("The column 'selDevUpperMeshSize' must be an integer")
+    }
     return(FALSE)
   }
-  if (is.integer(table$selDevLowerMeshSize)){
-    return(FALSE)
-  }
-  if (is.integer(table$selDevUpperMeshSize)){
-    return(FALSE)
-  }
-  if (is.character(table$AREA)){
-    return(FALSE)
-  }
+
+  #stop("Implement check on meshed gear / selectivitydevice. Given for all non-na gear / selectivitydevices, if given")
 
   return(TRUE)
 }
@@ -100,7 +134,7 @@ readMetierTable <- function(filename, encoding="UTF8"){
   loc$encoding <- encoding
   suppressMessages(mettab <- readr::read_delim(filename, delim="\t", comment = "#", locale = loc, trim_ws = T, skip_empty_rows = T, na = c("")))
 
-  columns <- c("metier", "gearcode", "target", "meshedGear", "lowerMeshSize", "upperMeshSize", "selectivityDevice", "meshedSelectivityDevice", "selDevLowerMeshSize", "selDevUpperMeshSize", "AREA")
+  columns <- c("metier", "gearcode", "target", "meshedGear", "lowerMeshSize", "upperMeshSize", "selectivityDevice", "meshedSelectivityDevice", "selDevLowerMeshSize", "selDevUpperMeshSize")
   for (co in columns){
     if (is.null(mettab[[co]])){
       mettab[[co]] <- NA
@@ -116,8 +150,7 @@ readMetierTable <- function(filename, encoding="UTF8"){
                                       selectivityDevice=as.character(mettab$selectivityDevice),
                                       meshedSelectivityDevice=as.logical(mettab$meshedSelectivityDevice),
                                       selDevLowerMeshSize=as.integer(mettab$selDevLowerMeshSize),
-                                      selDevUpperMeshSize=as.integer(mettab$selDevUpperMeshSize),
-                                      AREA=as.character(mettab$AREA)
+                                      selDevUpperMeshSize=as.integer(mettab$selDevUpperMeshSize)
                                       )
 
   nonmapped <- names(mettab)[!names(mettab) %in% names(mettab_dt)]
@@ -133,12 +166,25 @@ readMetierTable <- function(filename, encoding="UTF8"){
 #' @description checks that metiers are uniquely defined and that mesh-size ranges dont overlap
 #' @param metiertable \code{\link[RstoxFDA]{MetierTable}}
 #' @noRd
-checkMetierTable <- function(metiertable){
-  if (!is.MetierTable(metiertable)){
+checkMetierTable <- function(metiertable, target=!is.null(targetColumn), meshSize=!is.null(meshSizeColumn), selDev=!is.null(selectivityDeviceColumn), selDevMeshSize=!is.null(selectivityDeviceMeshSizeColumn)){
+  if (!is.MetierTable(metiertable, T)){
     stop("The provided metiertable is not correctly formatted (RstoxFDA::MetierTable).")
   }
 
-  metstring <- paste(metiertable$gearcode, metiertable$target, metiertable$lowerMeshSize, metiertable$upperMeshSize, metiertable$selectivityDevice, metiertable$selDevLowerMeshSize, metiertable$selDevUpperMeshSize, sep="_")
+  #duplicatecheck
+  metstring <- metiertable$gearcode
+  if (target){
+    metstring <- paste(metstring, metiertable$target, sep="_")
+  }
+  if (meshSize){
+    metstring <- paste(metstring, metiertable$lowerMeshSize, metiertable$upperMeshSize, sep="_")
+  }
+  if (selDev){
+    metstring <- paste(metstring, metiertable$selectivityDevice, sep="_")
+  }
+  if (selDevMeshSize){
+    metstring <- paste(metstring, metiertable$selDevLowerMeshSize, metiertable$selDevUpperMeshSize, sep="_")
+  }
 
   duplicates <- unique(metiertable$metier[duplicated(metstring)])
   if (length(duplicates) > 0){
@@ -172,16 +218,60 @@ checkMetierTable <- function(metiertable){
 
   # check that mesh sizes dont overlap for same gear
   meshedGears <- metiertable[metiertable$meshedGear,]
-  stop("Implement check for overlapping mesh ranges")
+  meshedGears$range <- paste(meshedGears$lowerMeshSize, meshedGears$upperMeshSize, sep="-")
+  for (g in unique(meshedGears$gearcode)){
+    ranges <- meshedGears[meshedGears$gearcode == g,]
+    ranges <- ranges[!duplicated(ranges$range),]
+
+    lower <- ranges$lowerMeshSize
+    upper <- ranges$upperMeshSize
+
+    ord <- order(lower)
+    lower <- lower[ord]
+    upper <- upper[ord]
+
+    incr <- c()
+    for (i in 1:length(lower)){
+      incr <- c(incr, lower[i], upper[i])
+    }
+
+    if (any(order(incr) != 1:(2*length(lower)))){
+      stop(paste("Mesh sizes have overlapping ranges for gear", g))
+    }
+
+  }
 
   # check that seldev mesh sizes dont overlap for same gear and selection device
-  stop("Implement check for overlapping seldev mesh ranges")
+  meshedSeldev <- metiertable[metiertable$meshedSelectivityDevice,]
+  meshedSeldev$range <- paste(meshedSeldev$selDevLowerMeshSize, meshedSeldev$selDevUpperMeshSize, sep="-")
+  meshedSeldev$gs <- paste(meshedSeldev$gearcode, meshedSeldev$selectivityDevice, sep="/")
+  for (g in unique(meshedSeldev$gs)){
+    ranges <- meshedSeldev[meshedSeldev$gs == g,]
+    ranges <- ranges[!duplicated(ranges$range),]
+
+    lower <- ranges$selDevLowerMeshSize
+    upper <- ranges$selDevUpperMeshSize
+
+    ord <- order(lower)
+    lower <- lower[ord]
+    upper <- upper[ord]
+
+    incr <- c()
+    for (i in 1:length(lower)){
+      incr <- c(incr, lower[i], upper[i])
+    }
+
+    if (any(order(incr) != 1:(2*length(lower)))){
+      stop(paste("Mesh sizes have overlapping ranges for selectivity device on a gear:", g))
+    }
+
+  }
 }
 
 #' Check if provided gears are OK to use with provided metiertable
 #' @noRd
 checkGear <- function(gearVector, metiertable){
-  missing <- gearVector[!(gearVector %in% metiertable$gearcode)]
+  missing <- gearVector[!is.na(gearVector) & !(gearVector %in% metiertable$gearcode)]
   if (length(missing) > 0){
     stop(paste("Metier is not defined for all gears. Missing: ", paste(missing, collapse=",")))
   }
@@ -190,46 +280,39 @@ checkGear <- function(gearVector, metiertable){
 #' Check if provided target are OK to use with provided metiertable
 #' @noRd
 checkTarget <- function(targetVector, metiertable){
-  missing <- targetVector[!(targetVector %in% metiertable$target)]
+  missing <- targetVector[!is.na(targetVector) & !(targetVector %in% metiertable$target)]
   if (length(missing) > 0){
-    stop(paste("Metier is not defined for all gears. Missing: ", paste(missing, collapse=",")))
+    stop(paste("Metier is not defined for all targets. Missing: ", paste(missing, collapse=",")))
   }
 }
 
-#' Issue warning if not all metier-areas exist in data
 #' @noRd
-checkArea <- function(areaVector, metiertable){
-  missing <- metiertable$AREA[!(metiertable$AREA %in% areaVector)]
-  if (length(missing)>0){
-    warning(paste("Area specific metier defintions provided for areas not in data, areas:", paste(missing, collapse=",")))
+checkSelectivityDevice <- function(selectivityDeviceVector, metiertable){
+  missing <- selectivityDeviceVector[!is.na(selectivityDeviceVector) & !(selectivityDeviceVector %in% metiertable$selectivityDevice)]
+  if (length(missing) > 0){
+    stop(paste("Metier is not defined for all selectivity devices Missing: ", paste(missing, collapse=",")))
   }
 }
-
-#' Check if provided mesh sizes are OK to use with provided metiertable
-#' @noRd
-checkMeshSize <- function(gearvector, meshSizeVector, metiertable){
-
-}
-
 
 #' Annotate metier
 #' @description annotates metier to data table
 #' @details
-#'  The parameter 'areaColumn' may be NULL which specifies that metiers are not dependent on area for the annotation.
-#'  If areaColumn is given, NA values in 'metiertable' specifies that metiers apply to all areas, so that area codes need only be provided for exceptions to this rule.
+#'  For the metier-defining variables 'gearcode', 'target' and 'selectivityDevice' (see \code{\link[RstoxFDA]{MetierTable}}), missing values are matches with missing values.
+#'  That is, a metier that is defined with a missing value for 'target'
+#'  will be annotated to otherwise matching data which has a missing value in the 'targetColumn'.
+#'
 #' @param data \code{\link[data.table]{data.table}} with data to be annotated
-#' @param metiertable \code{\link[RstoxFDA]} with metier definitions. Accepts all NAs for columns not used.
+#' @param metiertable \code{\link[RstoxFDA]{MetierTable}} with metier definitions. Accepts all NAs for columns not used.
 #' @param gearColumn character() identifies the column in 'data' that encodes gear. Gear definition must match metiertable$gearcode
 #' @param targetColumn character(), optional, identifies the column in 'data' that encodes target species. Definition must match metiertable$target
 #' @param meshSizeColumn integer(), optional, identifies the column in 'data' that encodes the mesh size of the gear.
 #' @param selectivityDeviceColumn character(), optional, identifies the column in 'data' that encodes selectivitydevices mounted on gear. Definition must mathc metiertable$selectivityDevice
 #' @param selectivityDeviceMeshSizeColumn integer(), optional, identifies the column in 'data' that encodes the mesh size of any mounted selectivity device.
 #' @param metierColName character() name of the column that should be appended to 'data'
-#' @param areaColumn character(), optional, identifies the column in 'data' that encodes areas for which different metierdefinitions are applicable.
 #' @param strict logical(), whether strict annotation should be applied (halting on missing definitions).
 #' @return \code{\link[data.table]{data.table}} 'data' with the column 'metierColName' appended (character).
 #' @export
-assignMetier <- function(data, metiertable, gearColumn, targetColumn=NULL, meshSizeColumn=NULL, selectivityDeviceColumn=NULL, selectivityDeviceMeshSizeColumn=NULL, areaColumn=NULL, metierColName="metier"){
+assignMetier <- function(data, metiertable, gearColumn, targetColumn=NULL, meshSizeColumn=NULL, selectivityDeviceColumn=NULL, selectivityDeviceMeshSizeColumn=NULL, metierColName="metier"){
 
   if (is.null(data)){
     stop("The parameter 'data' must be provided.")
@@ -242,12 +325,11 @@ assignMetier <- function(data, metiertable, gearColumn, targetColumn=NULL, meshS
   if (is.null(metiertable)){
     stop("The parameter 'metiertable' must be provided.")
   }
-  checkMetierTable(metiertable)
+  checkMetierTable(metiertable, target=!is.null(targetColumn), meshSize=!is.null(meshSizeColumn), selDev=!is.null(selectivityDeviceColumn), selDevMeshSize=!is.null(selectivityDeviceMeshSizeColumn))
 
   if (is.null(gearColumn)){
     stop("The parameter 'gearColumn' must be provided.")
   }
-
 
   if (is.null(metierColName)){
     stop("The parameter 'metierColName' must be provided.")
@@ -264,43 +346,44 @@ assignMetier <- function(data, metiertable, gearColumn, targetColumn=NULL, meshS
   if (!is.null(selectivityDeviceColumn)){
     checkSelectivityDevice(data[[selectivityDeviceColumn]], metiertable)
   }
-  if (!is.null(areaColumn)){
-    checkArea(data[[areaColumn]], metiertable)
-  }
-  if (!is.null(meshSizeColumn)){
-    checkMeshSize(data[[gearColumn]], data[[meshSizeColumn]], metiertable)
-  }
-  if (!is.null(selectivityDeviceMeshSizeColumn)){
-    checkMeshSizeSelDev(data[[gearColumn]], data[[selectivityDeviceColumn]], data[[selectivityDeviceMeshSizeColumn]], metiertable)
-  }
 
   #annotate
   for (i in 1:nrow(metiertable)){
-    selection <- data[[gearColumn]] == metiertable$gearcode[i]
+
+    selection <- !is.na(data[[gearColumn]]) & (data[[gearColumn]] == metiertable$gearcode[i])
     if (!is.null(targetColumn)){
-      selection <- selection & data[[targetColumn]] == metiertable$target[i]
+      if (is.na(metiertable$target[i])){
+        selection <- selection & is.na(data[[targetColumn]])
+      }
+      else{
+        selection <- selection & !is.na(data[[targetColumn]]) & data[[targetColumn]] == metiertable$target[i]
+      }
     }
     if (!is.null(selectivityDeviceColumn)){
-      selection <- selection & data[[selectivityDeviceColumn]] == metiertable$selectivityDevice[i]
+      if (is.na(metiertable$selectivityDevice[i])){
+        selection <- selection & is.na(data[[selectivityDeviceColumn]])
+      }
+      else{
+        selection <- selection & !is.na(data[[selectivityDeviceColumn]]) & data[[selectivityDeviceColumn]] == metiertable$selectivityDevice[i]
+      }
     }
-    if (!is.null(areaColumn) & !is.na(metiertable$AREA[i])){
-      selection <- selection & (data[[areaColumn]] == metiertable$AREA)
+    if (!is.null(meshSizeColumn) & !is.na(metiertable$gearcode[i]) & metiertable$meshedGear[i]){
+      selection <- selection & !is.na(data[[meshSizeColumn]]) & (data[[meshSizeColumn]] <= metiertable$upperMeshSize) & (data[[meshSizeColumn]] >= metiertable$lowerMeshSize)
     }
-    if (!is.null(meshSizeColumn) & metiertable$meshedGear[i]){
-      selection <- selection & (data[[meshSizeColumn]] <= metiertable$upperMeshSize) & (data[[meshSizeColumn]] >= metiertable$lowerMeshSize)
+    if (!is.null(selectivityDeviceMeshSizeColumn) & !is.na(metiertable$gearcode[i]) & !is.na(metiertable$selectivityDevice[i]) & metiertable$meshedSelectivityDevice[i]){
+      stop("Deal with NA in selectivityDeviceMeshSizeColumn")
+      selection <- selection & !is.na(data[[selectivityDeviceMeshSizeColumn]]) & (data[[selectivityDeviceMeshSizeColumn]] <= metiertable$selDevUpperMeshSize) & (data[[selectivityDeviceMeshSizeColumn]] >= metiertable$selDevLowerMeshSize)
     }
-    if (!is.null(selectivityDeviceMeshSizeColumn) & metiertable$meshedSelectivityDevice[i]){
-      selection <- selection & (data[[selectivityDeviceMeshSizeColumn]] <= metiertable$selDevUpperMeshSize) & (data[[selectivityDeviceMeshSizeColumn]] >= metiertable$selDevLowerMeshSize)
-    }
+
     data[selection, metierColName] <- metiertable$metier[i]
   }
 
   #report any missing metier definitions
-  missingMetier <- data[is.na(data[metierColName]),]
-  if (length(missingMetier) > 0){
-    cols <- c(gearColumn, targetColumn, selectivityDeviceColumn, areaColumn, meshSizeColumn, selectivityDeviceMeshSizeColumn)
+  missingMetier <- data[is.na(data[[metierColName]]),]
+  if (nrow(missingMetier) > 0){
+    cols <- c(gearColumn, targetColumn, selectivityDeviceColumn, meshSizeColumn, selectivityDeviceMeshSizeColumn)
     usedCols <- cols[cols %in% names(data)]
-    missingMetier <- unique(missingMetier[,usedCols])
+    missingMetier <- unique(missingMetier[,..usedCols])
     for (i in 1:nrow(missingMetier)){
       message(paste("Missing metier definition for:", paste(missingMetier[1,], collapse=" ")))
     }
--- tests/testthat/test-metierAnnotation.R
+++ tests/testthat/test-metierAnnotation.R
@@ -3,15 +3,45 @@ regularfile <- system.file("testresources","metiermapping_example.txt", package=
 metiertable <- readMetierTable(regularfile)
 expect_true(!any(is.na(metiertable$metier)))
 expect_true(!any(is.na(metiertable$gearcode)))
-expect_true(!any(is.na(metiertable$AREA)))
-expect_equal(sum(is.na(metiertable[1,])), 5)
+expect_equal(sum(is.na(metiertable[1,])), 3)
+expect_equal(sum(is.na(metiertable[6,])), 6)
 
 simplefile <- system.file("testresources","metiermapping_example_simple.txt", package="RstoxFDA")
 metiertable <- readMetierTable(simplefile)
 expect_true(!any(is.na(metiertable$metier)))
 expect_true(!any(is.na(metiertable$gearcode)))
-expect_true(!any(is.na(metiertable$AREA)))
 expect_equal(sum(is.na(metiertable[1,])), 8)
 
 errorfile <- system.file("testresources","metiermapping_example_error.txt", package="RstoxFDA")
 expect_error(readMetierTable(errorfile), "Some column names are not recognized: area")
+
+#test annotation
+testdata <- data.table::data.table(id=seq(1,6), gear=as.character(c(51,51,22,22,35,35)),
+                                   meshSize=c(90,90,90,120,NA,NA),
+                                   selDev=c("G","G",NA,NA,NA,NA),
+                                   selDevMS=c(110,90,NA,NA,NA,NA),
+                                   area=as.character(c("08","08","09","09","09","10")))
+
+regularfile <- system.file("testresources","metiermapping_example.txt", package="RstoxFDA")
+metiertable <- readMetierTable(regularfile)
+
+assignMetier(testdata, metiertable, "gear", meshSizeColumn = "meshSize", selectivityDeviceColumn = "selDev", selectivityDeviceMeshSizeColumn = "selDevMS")
+
+context("Annotation error checks")
+expect_error(assignMetier(testdata, metiertable, "gear"))
+
+#
+# tests
+#
+
+# check parsing of integers in char field
+
+# implement test for seldev mesh size given, when no selection device is given
+# implement test for seldev mesh size given, selection device is not meshed (logical)
+
+# implement test for mesh size given, when gear is not meshed (logical)
+# implement test for one range of mesh sizes is missing gear
+# implement test for one range of mesh sizes is missing seldev
+
+# implement test for partially specified meshedGear and meshed seldev
+
