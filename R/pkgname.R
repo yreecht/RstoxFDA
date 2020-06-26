2ccdc5d4f63261eb1940994f9fc20660c986eb66
edvinf <edvin.fuglebakk@gmail.com>
2020-06-26 15:01:56 +0200

    Rebuilt doc with RstoxBuild

--- R/pkgname.R
+++ R/pkgname.R
@@ -1,9 +1,10 @@
 #' Fisheries Dependent Analysis with RstoX
 #'
-#' This package is used to for fisheries dependent analysis. Including running the Estimated Catch at Age model through the Reca package developed by the Norwegian Computing Center.
+#' Fisheries dependent analysis, including running the Estimated Catch at Age model through the Reca package developed by the Norwegian Computing Center.
 #'
 #' The estimated catch at age (ECA) model uses the correlation structure in fisheries dependent data to distribute age readings from cathes (samples) onto the total reported landings. The ECA model is described in Hirst, D., Aanes, S., Storvik, G., Huseby, R. B., & Tvete, I. F. (2004). Estimating catch at age from market sampling data by using a Bayesian hierarchical model. Journal of the Royal Statistical Society: Series C (Applied Statistics), 53(1), 1-14.
 #' @docType package
 #' @name RstoxFDA
 #'
 "_PACKAGE"
+
